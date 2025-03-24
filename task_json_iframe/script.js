a4d5b12de7f084197c9984b8939ae258c726cda7
Bussa0112 <kbussa@aum.edu>
2025-03-23 23:04:26 -0500

    my dynamic page

--- task_json_iframe/script.js
+++ task_json_iframe/script.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const works = {
 
-        "task1": { "title": "Project 1", "url": "works/task1/index.html" },
-        "task2": { "title": "Project 2", "url": "works/task2/index.html" },
-        "task3": { "title": "Project 3", "url": "works/task3/index.html" },
-        "task4": { "title": "Project 4", "url": "works/task4/index.html" },
-        "task5": { "title": "Project 5", "url": "works/task5/index.html" }
+        "task1": { "title": "Task 1: Basic HTML Styling", "url": "works/task1/index.html" },
+        "task2": { "title": "Task 2: CSS Hover", "url": "works/task2/index.html" },
+        "task3": { "title": "Task 3: Welcome to JS", "url": "works/task3/index.html" },
+        "task4": { "title": "Task 4: Calculator with JS", "url": "works/task4/index.html" },
+        "task5": { "title": "Task 5: String Operation", "url": "works/task5/index.html" },
+        "task6": { "title": "Task 6: Conditions", "url": "works/task6/index.html" },
+        "task7": { "title": "Task 7: Call-Stack with Array", "url": "works/task7/index.html" },
+        "task8": { "title": "Task 8: Loops", "url": "works/task8/index.html", },
+        "task9": { "title": "Task 9: Class Work", "url": "works/task9/index.html" },
+        "task10": { "title": "Task 10: Class Work - Objects_2", "url": "works/task10/index.html" },
+        "task11": { "title": "Task 11: Functions", "url": "works/task11/index.html" },
+        "task12": { "title": "Task 12: Class & Bootstrap", "url": "works/task12/index.html" },
+        "task13": { "title": "Task 13: Class & Bootstrap", "url": "works/task13/index.html" },
+        "task14": { "title": "Task 14: Loops", "url": "works/task14/index.html" }
+
     };
     const projectList = document.getElementById('project-list');
     for (const key in works) {
